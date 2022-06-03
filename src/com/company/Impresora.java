12edb958645265b51d836242be13b647c4354313
Sitsoattila <101877141+Sitsoattila@users.noreply.github.com>
2022-06-03 14:08:46 +0100

    final commit

--- src/com/company/Impresora.java
+++ src/com/company/Impresora.java
@@ -3,8 +3,19 @@ package com.company;
 import java.util.ArrayList;
 
 public class Impresora {
-    boolean estado = true;
+    boolean estado = false;
     ArrayList<DocumentoOfimatico> listaDocumentos = new ArrayList<>();
+
+    public void encederImpresora(){
+
+        estado = true;
+
+    }
+
+    public void apagarImpresora(){
+        estado = false;
+
+    }
 
 
 }
